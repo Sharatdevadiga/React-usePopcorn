86601d4888f640930ece1f82eab78924028906af
Sharath <sharathdevadiga210@gmail.com>
2024-04-24 17:07:36 +0530

    components saperated

---
+++ src/components/MovieDetails.js
@@ -0,0 +1,110 @@
+import { useState, useEffect } from "react";
+import StarRating from "./StarRating";
+import ErrorMessage from "./ErrorMessage";
+import { KEY } from "../App V1";
+
+export default function MovieDetails({
+  selectedId,
+  onCloseMovie,
+  onAddWatched,
+}) {
+  const [movie, setMovie] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [userRating, setUsrRating] = useState("");
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Releases: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  function handleAdd() {
+    const newWatchedMovie = {
+      imdbId: selectedId,
+      title,
+      year,
+      poster,
+      imdbRating: Number(imdbRating),
+      runtime: Number(runtime.split(" ").at(0)),
+      userRating,
+    };
+    onAddWatched(newWatchedMovie);
+    onCloseMovie();
+  }
+
+  useEffect(
+    function () {
+      async function getMovieDetails() {
+        try {
+          setIsLoading(true);
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          );
+          const data = await res.json();
+          setMovie(data);
+          console.log(data);
+        } catch (err) {
+          console.error(error);
+          setError("Something went Wrong...");
+        } finally {
+          setIsLoading(false);
+        }
+      }
+      getMovieDetails();
+    },
+    [selectedId]
+  );
+  return error ? (
+    <ErrorMessage message={error} />
+  ) : (
+    <>
+      <div className="details">
+        <header>
+          <button className="btn-back" onClick={onCloseMovie}>
+            &larr;
+          </button>
+          <img src={poster} alt={`Poster of ${movie}`} />
+          <div className="details-overview">
+            <h2>{title}</h2>
+            <p>
+              {released} &bull; {runtime}
+            </p>
+            <p>{genre}</p>
+            <p>
+              <span>⭐</span>
+              {imdbRating} IMDB rating
+            </p>
+          </div>
+        </header>
+        <section>
+          <div className="rating">
+            <StarRating
+              maxRating={10}
+              size={24}
+              onSetRating={setUsrRating}
+            ></StarRating>
+
+            {userRating && (
+              <button className="btn-add" onClick={handleAdd}>
+                + Add to List
+              </button>
+            )}
+          </div>
+          <p>
+            <em>{plot}</em>
+          </p>
+          <p>Starrin {actors}</p>
+          <p>Directed by {director}</p>
+        </section>
+      </div>
+    </>
+  );
+}
